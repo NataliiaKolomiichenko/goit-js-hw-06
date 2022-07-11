f3d1ead1cb39ce6d86476cd850bccf50e731813d
NataliiaKolomiichenko <gella1996nr@gmail.com>
2022-07-11 21:39:12 +0300

    Added HEX displaying

--- js/task-09.js
+++ js/task-09.js
@@ -6,6 +6,9 @@ const bodyEl = document.querySelector('body');
 
 const changeColorBtn = document.querySelector('.change-color');
 
+const colorEl = document.querySelector('.color');
+
 changeColorBtn.addEventListener('click', () => {
   bodyEl.style.backgroundColor = getRandomHexColor()
+  colorEl.textContent = getRandomHexColor()
 });
